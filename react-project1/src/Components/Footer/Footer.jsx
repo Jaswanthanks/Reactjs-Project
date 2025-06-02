bd7d6dcde10153276dcaf18856ddddbfe664fa3e
Jaswanthanks <jaswanthanks@gmail.com>
2025-06-02 08:12:18 +0530

    first react commit

---
+++ react-project1/src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>@2025 Jaswanthan KS , All rights reserved</p>
+        <ul>
+            <li>Term of Services</li>
+            <li>Privacy Poicy</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Footer
